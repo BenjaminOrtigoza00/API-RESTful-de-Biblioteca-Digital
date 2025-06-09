f921959c430d0d1cd167b64a524ce42ba04951de
Benjamin Ortigoza <ortigozabenjamin100@gmail.com>
2025-06-09 18:09:06 -0300

    Coneccion a mongoDB

---
+++ src/config/mongo.ts
@@ -0,0 +1,16 @@
+import {connect}from "mongoose"
+
+process.loadEnvFile()
+
+
+const URI_DB= process.env .URI_DB || "mongodb://localhost:27017/coleccion"
+
+const connectDB = async ()=>{
+    try{
+        await connect(URI_DB)
+        console.log("Servidor Mongo DB conectado")
+        } catch (error){
+            console.log("Error al conectarse al servidor Mongo DB" )
+        }
+}
+export {connectDB}
